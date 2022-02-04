8ee77d82795ffd83b8461e76d4b95dc4b9aa7176
Inderpal Singh <inderpalsingh6763@gmail.com>
2022-02-04 17:24:11 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import { hot } from "react-hot-loader";
+import "./style.css";
+
+export const App = () => {
+	return <div>Hello world</div>;
+};
+
+// export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: { presets: ["@babel/env", "@babel/preset-react"] },
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	resolve: { extensions: ["*", ".js", ".jsx"] },
+	devServer: {
+		port: 3000,
+		hot: true,
+		open: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public/index.html"),
+			filename: "index.html",
+		}),
+	],
+};
